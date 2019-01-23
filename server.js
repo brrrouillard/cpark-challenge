4fe08e0dd2b6bfbacdfe27ccd5986fbfaae670c5
Matthieu Brouillard <mbrouilla@gmail.com>
2019-01-23 15:03:55 +0100

    Add Mongoose connexion

--- server.js
+++ server.js
@@ -6,6 +6,21 @@ const mongoose = require("mongoose");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+const mongoURI = require("./config/keys").mongoURI;
+
+// DATABASE CONNEXION
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("Mongoose connected"))
+  .catch(err => console.log(err));
+
+// MIDDLEWARES
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
